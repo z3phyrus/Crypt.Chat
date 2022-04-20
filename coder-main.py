508ad5cdf829bc621f0142ae8f4bd326a77e9c27
C043R <96727508+C043R@users.noreply.github.com>
2022-04-20 15:37:27 +0300

    Update coder-main.py

--- coder-main.py
+++ coder-main.py
@@ -1,45 +1,33 @@
-# Client for Crypt.Chat 
-# Basic / From tutorial 
 
+# Client for Crypt.Chat 
+from checks import *
 import socket 
 import threading 
 import requests
 
-# Checking internet connection , if active then continue 
-def internet_connection():
-    print("[+] Checking internet connection ....")
-    try:
-        request = requests.get('https://google.com', timeout=3)
-        print("Connection established")
-    except (requests.ConnectionError, requests.Timeout) as exception:
-        exit("[!] No internet connection.")
-
 internet_connection()
 
-nickname = input("Enter a nickname > ")
-# Can use config.ini files to store the nickname so the user wont have to input it all the time 
-# When the user wants to chainge their nickname we can have a keywork in chat that asks for a new nickname and changes the value in config.ini
+nickname = input(Colors.cyan+"[+] Enter a nickname > ")
 
 
-client = socket.socket(socket.AF_INET ,socket.SOCK_STREAM) 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(("127.0.0.1",1234))
+client.connect(("127.0.0.1", 55555))
 
 
 def receive_data():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('utf-8')
         
             if message == "NICK": 
-                client.send(nickname.encode("ascii"))
+                client.send(nickname.encode("utf-8"))
             else:
-                print(message)
+                print(f'[!] {Colors.green}> {Colors.green}{message}')
         except:
-            print("Error in connection")
-            # Can use a function to keep the chat on till conneciton is back  and displaying an error symbol 
-            client.close() 
-            break 
+            print(Colors.red+"[+] Error in connection ...")
+            client.close()
+            break
 
 def writing():
     while True:
@@ -55,6 +43,3 @@ receive_thread.start()
 
 writing_thread = threading.Thread(target=writing)
 writing_thread.start()
-
-
-# So far the user can only close client or write & send new messages 
